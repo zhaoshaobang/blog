aad6e9476adb22686741914e7d8c04dc299b89fb
zhaoshaobang <zhaoshaobang@163.com>
2015-06-05 09:41:01 +0800

    task00001
    add the finished pages

---
+++ task0001/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+/*if ($_GET['username']=='zhaoshaobang' && $_GET['password']==123456) {
+    echo "成功";
+}*/
+echo "成功";
+?>
+</body>
+</html>
